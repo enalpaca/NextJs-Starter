85a80f1300f4d23b9a9c1cf37da9b03c83791630
Enalpaca <banhut.dev@gmail.com>
2022-12-07 15:24:45 +0700

    firt commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../utils/theme";
+import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+const clientSideEmotionCache = createEmotionCache();
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache, }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-
-
+export default MyApp;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,16 @@
-
+import { Container, Box, FormControl, InputLabel, Input, InputAdornment, TextField, Grid, IconButton, Typography } from "@mui/material";
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import KeyIcon from '@mui/icons-material/Key';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import React, { useState } from "react";
 export default function Signin() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const mainDivStyle = {
-    padding: "1em",
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
   };
   // function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
   //   // throw new Error("Function not implemented.");
@@ -19,85 +25,142 @@ export default function Signin() {
   //   )
   // }
   return (
-    // <div>
-    //   <div className="alert alert-primary" role="alert">
-    //     A simple primary alert—check it out!
-    //   </div>
-    //   <form onSubmit={(e) => handleSubmit(e)} action="/api/sendatalogin" method="get">
-    //     <h1 className="h3 mb-3 fw-normal">Đăng nhập</h1>
-    //     <label htmlFor="username"> Username </label>
-    //     <hr />
-    //     <input
-    //       type="text"
-    //       name="username"
-    //       id="username"
-    //       onChange={(e) => setUsername(e.target.value)}
-    //     />
+    <Container maxWidth="sm">
+      <Typography variant="h6" align="center"> Đăng nhập</Typography>
+      <Box sx={{ '& > :not(style)': { m: 1 } }}>
+        {/* 
+        grid-- container spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: '100vh' }}
+        box-- display="flex" alignItems="center" justifyContent="center"
+        grid first --
+                
+*/}
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Phone number:
+            </InputLabel>
+            <Input
+              id="input-with-icon-adornment"
+              placeholder="Please enter phone enter"
+              startAdornment={
+                <InputAdornment position="start">
+                  <PhoneAndroidIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Grid >
 
-    //     <hr />
-    //     <label htmlFor="password"> Password</label>
-    //     <hr />
-    //     <input
-    //       type="text"
-    //       name="password"
-    //       id="password"
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Password:
+            </InputLabel>
+            <Input
+              id="input-with-icon-adornment"
+              placeholder="Please enter password"
+              type={showPassword === true ? "text" : "password"}
+              startAdornment={
+                <InputAdornment position="start">
+                  <KeyIcon />
+                </InputAdornment>
+              }
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton onClick={handleClickShowPassword}>
+                    {showPassword === true ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Grid>
 
-    //     <hr />
-    //     <button type="submit"> Log in </button>
-    //   </form>
-    // </div>
-    <div className="container">
-      <div className="row">
-        <div className="col-3"></div>
-        <div className="col-6">
-          <h2>Đăng Nhập</h2>
-          <form action="#">
-            {/* So dien thoai */}
-            <div>
-              iconphone
-            </div>
-            <div>
-              <label htmlFor="#">Số điện thoại</label>
-              <input type="text" />
-            </div>
-
-            {/* Nhap mat khau */}
-            <div>
-              <div>
-                iconkey
-              </div>
-              <div>
-                <label htmlFor="#">Mật khẩu</label>
-                <input type="password" />
-                iconeye-to-hidden-or-show-password
-              </div>
-            </div>
-
-            <h5>Quên mật khẩu?</h5>
-
-            <button> Đăng nhập </button>
-
-            <span>hoặc</span>
-
-            <div>
-              iconfacebook-
-              <span>Facebook</span>
-              icongoogle-
-              <span>Google</span>
-            </div>
-          </form>
-
-          <hr />
-
-          <p> Bạn chưa có tài khoản?<a href="#">Đăng ký ngay</a></p>
-        </div>
-        <div className="col-3"></div>
-      </div>
-    </div >
-
+      </Box >
+    </Container >
   );
+  // <div>
+  //   <div className="alert alert-primary" role="alert">
+  //     A simple primary alert—check it out!
+  //   </div>
+  //   <form onSubmit={(e) => handleSubmit(e)} action="/api/sendatalogin" method="get">
+  //     <h1 className="h3 mb-3 fw-normal">Đăng nhập</h1>
+  //     <label htmlFor="username"> Username </label>
+  //     <hr />
+  //     <input
+  //       type="text"
+  //       name="username"
+  //       id="username"
+  //       onChange={(e) => setUsername(e.target.value)}
+  //     />
+
+  //     <hr />
+  //     <label htmlFor="password"> Password</label>
+  //     <hr />
+  //     <input
+  //       type="text"
+  //       name="password"
+  //       id="password"
+  //       onChange={(e) => setPassword(e.target.value)}
+  //     />
+
+  //     <hr />
+  //     <button type="submit"> Log in </button>
+  //   </form>
+  // </div>
+  // <div className="container">
+  //   <div className="row">
+  //     <div className="col-3"></div>
+  //     <div className="col-6">
+  //       <h2>Đăng Nhập</h2>
+  //       <form action="#">
+  //         {/* So dien thoai */}
+  //         <div>
+  //           iconphone
+  //         </div>
+  //         <div>
+  //           <label htmlFor="#">Số điện thoại</label>
+  //           <input type="text" />
+  //         </div>
+
+  //         {/* Nhap mat khau */}
+  //         <div>
+  //           <div>
+  //             iconkey
+  //           </div>
+  //           <div>
+  //             <label htmlFor="#">Mật khẩu</label>
+  //             <input type="password" />
+  //             iconeye-to-hidden-or-show-password
+  //           </div>
+  //         </div>
+
+  //         <h5>Quên mật khẩu?</h5>
+
+  //         <button> Đăng nhập </button>
+
+  //         <span>hoặc</span>
+
+  //         <div>
+  //           iconfacebook-
+  //           <span>Facebook</span>
+  //           icongoogle-
+  //           <span>Google</span>
+  //         </div>
+  //       </form>
+
+  //       <hr />
+
+  //       <p> Bạn chưa có tài khoản?<a href="#">Đăng ký ngay</a></p>
+  //     </div>
+  //     <div className="col-3"></div>
+  //   </div>
+  // </div >
+
 }
 
 // function handleGetUser(): void {
