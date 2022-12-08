93899e9937b8ff685832f2bfc69fcac2005c650a
Enalpaca <banhut.dev@gmail.com>
2022-12-08 15:43:39 +0700

    first step complete  UI

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { Container, Box, FormControl, InputLabel, Input, InputAdornment, TextField, Grid, IconButton, Typography, Button, Link, Paper, Divider } from "@mui/material";
+import { Container, Box, FormControl, InputLabel, Input, InputAdornment, TextField, Grid, IconButton, Typography, Button, Link, Paper, Divider, } from "@mui/material";
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import { styled } from '@mui/material/styles';
 import KeyIcon from '@mui/icons-material/Key';
@@ -15,18 +15,27 @@ export default function Signin() {
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword)
   };
-  // function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
-  //   // throw new Error("Function not implemented.");
-  //   console.log(
-  //     'Handle Submit'
-  //   )
-  //   console.log(
-  //     'username', username
-  //   )
-  //   console.log(
-  //     'password', password
-  //   )
-  // }
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     phone: data.get('phone'),
+  //     password: data.get('password'),
+  //   })
+  // };
+
+  function handleSubmit(): void {
+    // throw new Error("Function not implemented.");
+    console.log(
+      'Handle Submit'
+    )
+    console.log(
+      'username', username
+    )
+    console.log(
+      'password', password
+    )
+  }
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
@@ -43,116 +52,167 @@ export default function Signin() {
     textAlign: "center"
   }
   return (
-    <Container component="main" maxWidth="xs" >
-      {/* Sign in */}
+    <>
       <Box sx={{
-        marginTop: 1, display: 'flex', flexDirection: 'column', alignItems: 'center'
-      }}
+        marginTop: 9,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }
+      }
       >
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ backgroundColor: '#2e3030', color: 'white' }} >
+        <Container component="main" maxWidth="xs">
+          <Box component="form" onSubmit={handleSubmit} noValidate>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} >
 
-          <Grid item xs={12} style={{ textAlign: 'center' }} >
-            <Typography component="h1" variant="h5" align="center"> Đăng nhập</Typography>
-          </Grid>
-
-          {/* Phone Number */}
-          <Grid item xs={12} style={{ textAlign: 'center' }} >
-            <FormControl variant="standard" margin="normal" fullWidth>
-              <InputLabel htmlFor="input-with-icon-adornment" style={{ color: 'white' }}>
-                Phone number:
-              </InputLabel>
-              <Input
-                id="input-with-icon-adornment"
-                placeholder="Please enter phone enter"
-                startAdornment={
-                  <InputAdornment position="start" >
-                    <PhoneAndroidIcon style={{ color: 'white' }} />
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-          </Grid>
-
-
-          {/* Password */}
-          <Grid item xs={12} style={{ textAlign: 'center' }}>
-            <FormControl variant="standard" margin="normal" fullWidth>
-              <InputLabel htmlFor="input-with-icon-adornment" style={{ color: 'white' }}>
-                Password:
-              </InputLabel>
-              <Input
-                id="input-with-icon-adornment"
-                placeholder="Please enter password"
-                type={showPassword === true ? "text" : "password"}
-                startAdornment={
-                  <InputAdornment position="start">
-                    <KeyIcon style={{ color: 'white' }} />
-                  </InputAdornment>
-                }
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton onClick={handleClickShowPassword}>
-                      {showPassword === true ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-          </Grid>
-
-
-          {/* Forgot password */}
-          <Grid item xs={12}>
-            <Grid item xs>
-              <Link href="#" variant="body2" style={{ color: 'white' }}> Quên mật khẩu? </Link>
-            </Grid>
-          </Grid>
-
-
-          {/* Button sign in */}
-          <Grid item xs={12} style={{ textAlign: 'center' }}>
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} style={{ backgroundColor: 'white' }} >
-              Đăng nhập
-            </Button>
-          </Grid>
-
-
-          <Grid item xs={12} style={{ textAlign: 'center' }}>
-            <div style={{ textAlign: 'center', borderColor: 'white' }}>
-              <Divider style={{ color: 'white' }}>hoặc</Divider>
-            </div>
-          </Grid>
-
-          {/* Sing in with Facebook or Google */}
-          <Grid item xs={12} style={{ textAlign: 'center' }}>
-            <Grid container spacing={2}>
-              <Grid item xs={6} md={6}>
-                <Button type="submit" fullWidth variant="outlined" style={{ color: 'white' }} href="#as-link"><FacebookOutlinedIcon style={{ color: '#0543ad' }} />Facebook </Button>
+              <Grid item xs={12} style={{ textAlign: 'center' }} sx={{ m: 1 }}>
+                <Typography component="h1" variant="h4" align="center"> Đăng nhập</Typography>
               </Grid>
-              <Grid item xs={6} md={6}>
-                <Button type="submit" fullWidth variant="outlined" style={{ color: 'white' }} href="#as-link" ><GoogleIcon style={{ color: 'red' }} />Google</Button>
+
+
+
+              {/* <Grid item xs={12} style={{ textAlign: 'center' }} >
+                <FormControl variant="standard" margin="normal" fullWidth >
+                  <TextField
+                    label="Please enter OTP"
+                    placeholder="OTP"
+                    type="tel"
+                    helperText="OTP incorect"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <PhoneAndroidIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+
+                    
+                  // startAdornment={
+                  //   <InputAdornment position="start" >
+                  //     <PhoneAndroidIcon />
+                  //   </InputAdornment>
+                  // }
+
+                  // sx={{
+                  //   "& fieldset": { borderTop: 'none', borderRight: 'none', borderLeft: 'none' },
+                  // }}
+                  />
+                </FormControl>
+              </Grid> */}
+
+
+
+
+              {/* Phone Number */}
+              <Grid item xs={12} style={{ textAlign: 'center' }} >
+                <FormControl variant="standard" margin="normal" fullWidth >
+                  <InputLabel htmlFor="input-with-icon-adornment" >
+                    {/* Phone number: */}
+                  </InputLabel>
+                  <Input
+                    id="input-with-icon-adornment"
+                    placeholder="Số điện thoại"
+                    startAdornment={
+                      <InputAdornment position="start" >
+                        <PhoneAndroidIcon />
+                      </InputAdornment>
+                    }
+                    onChange={(event) => setUsername(event.target.value)}
+
+                  />
+                </FormControl>
               </Grid>
+
+
+              {/* Password */}
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <FormControl variant="standard" margin="normal" fullWidth>
+                  <InputLabel htmlFor="input-with-icon-adornment" >
+                    {/* Password: */}
+                  </InputLabel>
+                  <Input
+                    id="input-with-icon-adornment"
+                    placeholder="Mật khẩu"
+                    type={showPassword === true ? "text" : "password"}
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <KeyIcon />
+                      </InputAdornment>
+                    }
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClickShowPassword}>
+                          {showPassword === true ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </FormControl>
+              </Grid>
+
+
+              {/* Forgot password */}
+              <Grid item xs={12}>
+                <Grid item xs>
+                  <Link href="#" variant="body2" > Quên mật khẩu? </Link>
+                </Grid>
+              </Grid>
+
+
+              {/* Button sign in */}
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Button type="button" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} onClick={() => handleSubmit()}>
+                  Đăng nhập
+                </Button>
+              </Grid>
+
+
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <div style={{ textAlign: 'center' }}>
+                  <Divider >hoặc</Divider>
+                </div>
+              </Grid>
+
+              {/* Sing in with Facebook or Google */}
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6} md={6}>
+                    <Button type="submit" fullWidth variant="outlined" href="#as-link"><FacebookOutlinedIcon style={{ color: '#0543ad' }} />Facebook </Button>
+                  </Grid>
+                  <Grid item xs={6} md={6}>
+                    <Button type="submit" fullWidth variant="outlined" href="#as-link" ><GoogleIcon style={{ color: 'red' }} />Google</Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+
+              {/* Horizonal */}
+              <Grid item xs={12} style={{ textAlign: 'center' }} >
+                <Divider />
+              </Grid>
+
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                Bạn chưa có tài khoản? <Link href="#" variant="body2">Đăng ký ngay.</Link>
+              </Grid>
+            </Grid >
+
+
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+
             </Grid>
-          </Grid>
+          </Box>
+        </Container>
 
-
-          {/* Horizonal */}
-          <Grid item xs={12} style={{ textAlign: 'center' }} >
-            <Divider style={{ borderColor: 'White' }} />
-          </Grid>
-
-          <Grid item xs={12} style={{ textAlign: 'center' }}>
-            Bạn chưa có tài khoản? <Link href="#" variant="body2" style={{ color: 'white' }}>Đăng ký ngay.</Link>
-          </Grid>
-        </Grid >
-
-
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-
-        </Grid>
 
       </Box >
-    </Container >
+    </>
+    // <Container component="main" maxWidth="xs" >
+    //   {/* Sign in */}
+
+
+    // </Container >
   );
   // <div>
   //   <div className="alert alert-primary" role="alert">
